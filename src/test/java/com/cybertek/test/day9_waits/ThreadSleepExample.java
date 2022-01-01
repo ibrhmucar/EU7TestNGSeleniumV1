4090e7a4746ff0726b2974671fb158ea11ced317
ibrahimucar <ibrhmucar@gmail.com>
2022-01-01 23:59:23 +0300

    changed

--- src/test/java/com/cybertek/test/day9_waits/ThreadSleepExample.java
+++ src/test/java/com/cybertek/test/day9_waits/ThreadSleepExample.java
@@ -4,9 +4,13 @@ import com.cybertek.utilities.WebDriverFactory;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Point;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class ThreadSleepExample {
     WebDriver driver;
